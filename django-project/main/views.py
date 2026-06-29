ee785fc6010c5c5bfe05bee26ca8badd061b8dd4
raushanhan <raushanhan2808@icloud.com>
2026-06-29 14:36:33 +0300

    изучаем функцию render

--- django-project/main/views.py
+++ django-project/main/views.py
@@ -1,11 +1,30 @@
 from django.shortcuts import render
-
-# Create your views here.
-
+from django.http import Http404
 
 from django.http import HttpResponse
 
+COURSES = {
+    "Python": "Здесь изучаем питон",
+    "plusy": "Здесь изучаем плюсы",
+    "Super_course": "Здесь изучаем что такое супер",
+    "Tatar_language": "учим татарский"
+}
+
 def main_page(request):
-    return HttpResponse("Добро пожаловать на официальный сайт лагеря Квант!")
 
+    return render(request, './base.html', context={'courses': list(COURSES.items())})
 
+def course_description(request, course_name):
+
+    # if course_name not in COURSES:
+    #     raise Http404(f"курса {course_name} нет")
+
+    if course_name not in COURSES:
+        return HttpResponse(
+            f"курса {course_name} нет",
+            status=404
+        )
+
+    return HttpResponse(
+        COURSES[course_name]
+    )
--- lessons/28-06-django-start/code/main/views.py
+++ lessons/28-06-django-start/code/main/views.py
@@ -4,8 +4,3 @@ from django.shortcuts import render
 
 
 from django.http import HttpResponse
-
-def main_page(request):
-    return HttpResponse("Добро пожаловать на официальный сайт лагеря Квант!")
-
-
